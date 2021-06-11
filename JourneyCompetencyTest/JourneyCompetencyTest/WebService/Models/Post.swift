4a7522a74f94fb71ef56d546da81754acd14ec5b
ravina <ravina.kaniyawala@gmail.com>
2021-06-11 20:35:53 +1200

    post model + storyBoard

---
+++ JourneyCompetencyTest/JourneyCompetencyTest/WebService/Models/Post.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  JourneyCompetencyTest
+//
+//  Created by Riya Kaniyawala on 11/06/21.
+//
+
+import Foundation
+import ObjectMapper
+
+class Post: Mappable {
+    
+    var body: String?
+    var id: Int?
+    var title: String?
+    var userId: Int?
+    
+    required init?(map: Map) {
+        mapping(map: map)
+    }
+    
+    func mapping(map: Map) {
+    body <- map["body"]
+    id <- map["id"]
+    title <- map["title"]
+    userId <- map["userId"]
+    }
+    
+}
