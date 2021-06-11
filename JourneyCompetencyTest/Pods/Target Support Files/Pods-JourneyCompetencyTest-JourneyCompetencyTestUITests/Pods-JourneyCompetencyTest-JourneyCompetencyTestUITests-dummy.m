776b6aa1312c8c671295e9215e621cc6f9575659
ravina <ravina.kaniyawala@gmail.com>
2021-06-11 20:17:09 +1200

    pod install

---
+++ JourneyCompetencyTest/Pods/Target Support Files/Pods-JourneyCompetencyTest-JourneyCompetencyTestUITests/Pods-JourneyCompetencyTest-JourneyCompetencyTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JourneyCompetencyTest_JourneyCompetencyTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_JourneyCompetencyTest_JourneyCompetencyTestUITests
+@end
